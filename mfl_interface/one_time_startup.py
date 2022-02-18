d4fea61bd6cf8360f86be88ef8c86b0695fd0a05
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-02-18 18:38:07 +0300

    updated gitignore

--- mfl_interface/one_time_startup.py
+++ mfl_interface/one_time_startup.py
@@ -47,34 +47,36 @@ def create_db():
     cursorObject.close()
     dataBase.close()'''
 
-    env = environ.Env()
-    environ.Env.read_env()
+    # env = environ.Env()
+    # environ.Env.read_env()
+    #
+    # dataBase = mysql.connector.connect(
+    #     host="localhost",
+    #     user=env("DATABASE_USER"),
+    #     passwd=env("DATABASE_PASSWORD")
+    # )
+    #
+    # # preparing a cursor object
+    # cursorObject = dataBase.cursor()
+    # cursorObject.execute("USE mfl_interface_db")
+    #
+    # f = open(os.path.join(Path(__file__).resolve().parent.parent, "facilities/test/sample.json"), 'r')
+    # data = json.load(f)
+    #
+    # #for i in data['results']:
+    # #    print(data['results'][1]["code"])
+    # code = data['results'][1]['code']
+    # name = data['results'][1]['name']
+    # lon = data['results'][1]['lat_long'][1]
+    # add_facility_data = "insert into facilities_facility_info(id, mfl_code, name, county_id, lon, partner_id, owner_id) value(uuid.uuid4(), %s, %s, %s, %s, %s, %s)"
+    # data = (data['results'][1]["code"], data['results'][1]["name"], 30, data['results'][1]["lat_long"][1],
+    #         1, 1)
+    # cursorObject.execute(add_facility_data, data)
+    # dataBase.commit()
+    #
+    # f.close()
+    #
+    # cursorObject.close()
+    # dataBase.close()
 
-    dataBase = mysql.connector.connect(
-        host="localhost",
-        user=env("DATABASE_USER"),
-        passwd=env("DATABASE_PASSWORD")
-    )
-
-    # preparing a cursor object
-    cursorObject = dataBase.cursor()
-    cursorObject.execute("USE mfl_interface_db")
-
-    f = open(os.path.join(Path(__file__).resolve().parent.parent, "facilities/test/sample.json"), 'r')
-    data = json.load(f)
-
-    #for i in data['results']:
-    #    print(data['results'][1]["code"])
-    code = data['results'][1]['code']
-    name = data['results'][1]['name']
-    lon = data['results'][1]['lat_long'][1]
-    add_facility_data = "insert into facilities_facility_info(id, mfl_code, name, county_id, lon, partner_id, owner_id) value(uuid.uuid4(), %s, %s, %s, %s, %s, %s)"
-    data = (data['results'][1]["code"], data['results'][1]["name"], 30, data['results'][1]["lat_long"][1],
-            1, 1)
-    cursorObject.execute(add_facility_data, data)
-    dataBase.commit()
-
-    f.close()
-
-    cursorObject.close()
-    dataBase.close()
+    return 0
