5f7e55f4facf88d4e5ca8665d784c7829e2fffe0
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-02-20 23:40:16 +0300

    Update preloaded data and .env

--- mfl_interface/settings.py
+++ mfl_interface/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': 'his-list_mysql_1',
+        'HOST': env("DATABASE_HOST"),
         'PORT': '3306',
     }
 }
