50e86b3c9267c56d643e1a423b76b4275354cd31
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-04-20 15:32:47 +0300

    updated users module

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.test, name='test'),
-    # path('signin/', views.signin, name='signin'),
+    path('', views.redirect_to_frontend, name='redirect_to_frontend'),
+    path('signin/', views.signin, name='signin'),
     # path('connect/authorize/callback/', views.signup_callback, name='signup_callback'),
-    # path('signin-oidc', views.signin_oidc, name='signin_oidc'),
-    # path('logout_user/', views.logout_user, name='logout_user'),
+    path('signin-oidc', views.signin_oidc, name='signin_oidc'),
+    path('logout_user/', views.logout_user, name='logout_user'),
     path('store_tokens', views.store_tokens, name='store_tokens'),
-    # path('log_user_in', views.log_user_in, name='log_user_in'),
+    path('log_user_in', views.log_user_in, name='log_user_in'),
 ]
 
--- users/views.py
+++ users/views.py
@@ -14,34 +14,34 @@ environ.Env.read_env()
 
 
 
-#
-# def signin(request):
-#     authority= "https://auth.kenyahmis.org/DwhIdentity"
-#     client_id= "dwh.his"
-#     redirect_uri= "http://localhost:8000/signin-oidc"
-#     response_type= "id_token token"
-#     scope= "openid profile apiApp"
-#     post_logout_redirect_uri= "http://localhost:8000"
-#     state= generate_nonce()
-#     nonce= generate_nonce()
-#
-#     ReturnUrl = '?ReturnUrl=%2FDwhIdentity%2Fconnect%2Fauthorize%2Fcallback'
-#     client_id = '%3Fclient_id%3D'+client_id
-#     url = 'https://auth.kenyahmis.org/DwhIdentity/Account/Login'+ReturnUrl+client_id
-#
-#     auth_token_url = url+'%26redirect_uri%3D'+redirect_uri+'%26response_type%3Did_token%2520token%26scope%3Dopenid%2520profile%2520apiApp%26' \
-#                                                        'state%3D'+state+'%26nonce%3D'+nonce
-#
-#     return redirect(auth_token_url)
-#
-#
-# def signin_oidc(request):
-#     # allows frontend to consume access token and return to backen
-#     return render(request, 'users/signin_oidc.html')
-#
 
-def test(request):
-    return 0
+def signin(request):
+    authority= "https://auth.kenyahmis.org/DwhIdentity"
+    client_id= "dwh.his"
+    redirect_uri= "http://localhost:8000/signin-oidc"
+    response_type= "id_token token"
+    scope= "openid profile apiApp"
+    post_logout_redirect_uri= "http://localhost:8000"
+    state= generate_nonce()
+    nonce= generate_nonce()
+
+    ReturnUrl = '?ReturnUrl=%2FDwhIdentity%2Fconnect%2Fauthorize%2Fcallback'
+    client_id = '%3Fclient_id%3D'+client_id
+    url = 'https://auth.kenyahmis.org/DwhIdentity/Account/Login'+ReturnUrl+client_id
+
+    auth_token_url = url+'%26redirect_uri%3D'+redirect_uri+'%26response_type%3Did_token%2520token%26scope%3Dopenid%2520profile%2520apiApp%26' \
+                                                       'state%3D'+state+'%26nonce%3D'+nonce
+
+    return redirect(auth_token_url)
+
+
+def signin_oidc(request):
+    # allows frontend to consume access token and return to backen
+    return render(request, 'users/signin_oidc.html')
+
+
+def redirect_to_frontend(request):
+    return JsonResponse({'status': 'success'})
 
 
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
@@ -56,53 +56,53 @@ def store_tokens(request):
         request.session["session_state"] = request.POST.get('session_state')
 
     return JsonResponse({'status': 'success'})
-#
-# def log_user_in(request):
-#
-#     headers = CaseInsensitiveDict()
-#     headers["Accept"] = "application/json"
-#     headers["Authorization"] = "Bearer " + request.session["access_token"]
-#     url = 'https://auth.kenyahmis.org/DwhIdentity/connect/userinfo'
-#     response = requests.get(url, headers=headers, verify=False)
-#     print('response ====================>', response.status_code)
-#
-#     fullname = json.loads(response.content.decode('utf-8'))['FullName']
-#     organization = json.loads(response.content.decode('utf-8'))['OrganizationId']
-#     email = json.loads(response.content.decode('utf-8'))['email']
-#     request.session["logged_in_username"] = fullname
-#     request.session["logged_in_user_org"] = organization
-#     request.session["logged_in_user_email"] = email
-#     print(json.loads(response.content.decode('utf-8'))['FullName'])
-#     print('what is the email',json.loads(response.content.decode('utf-8'))['email'], request.session["logged_in_user_email"])
-#
-#     return HttpResponseRedirect('/')
+
+def log_user_in(request):
+
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    headers["Authorization"] = "Bearer " + request.session["access_token"]
+    url = 'https://auth.kenyahmis.org/DwhIdentity/connect/userinfo'
+    response = requests.get(url, headers=headers, verify=False)
+    print('response ====================>', response.status_code)
+
+    fullname = json.loads(response.content.decode('utf-8'))['FullName']
+    organization = json.loads(response.content.decode('utf-8'))['OrganizationId']
+    email = json.loads(response.content.decode('utf-8'))['email']
+    request.session["logged_in_username"] = fullname
+    request.session["logged_in_user_org"] = organization
+    request.session["logged_in_user_email"] = email
+    print(json.loads(response.content.decode('utf-8'))['FullName'])
+    print('what is the email',json.loads(response.content.decode('utf-8'))['email'], request.session["logged_in_user_email"])
+
+    return HttpResponseRedirect('/')
 
 
-#
-# def generate_nonce(length=32):
-#     """Generate pseudorandom number."""
-#     return ''.join([str(random.randint(0, 9)) for i in range(length)])
-#
-#
-# def logout_user(request):
-#     # headers = CaseInsensitiveDict()
-#     # headers["Accept"] = "application/json"
-#     # headers["Authorization"] = "Bearer " + request.session["access_token"]
-#     redirect_url = 'http://localhost:8000'
-#     url = 'https://auth.kenyahmis.org/DwhIdentity/connect/endsession?id_token_hint='+request.session["id_token"]+ \
-#                                '&post_logout_redirect_uri=' + redirect_url
-#     response = requests.get(url, verify=False)
-#     print('response ====================>', response.status_code, response.content)
-#
-#     try:
-#         del request.session['logged_in_username']
-#         del request.session["logged_in_user_org"]
-#         del request.session["logged_in_user_email"]
-#         del request.session["access_token"]
-#         del request.session["id_token"]
-#         del request.session["state"]
-#         del request.session["session_state"]
-#     except KeyError:
-#         pass
-#
-#     return HttpResponseRedirect(url)
+
+def generate_nonce(length=32):
+    """Generate pseudorandom number."""
+    return ''.join([str(random.randint(0, 9)) for i in range(length)])
+
+
+def logout_user(request):
+    # headers = CaseInsensitiveDict()
+    # headers["Accept"] = "application/json"
+    # headers["Authorization"] = "Bearer " + request.session["access_token"]
+    redirect_url = 'http://localhost:8000'
+    url = 'https://auth.kenyahmis.org/DwhIdentity/connect/endsession?id_token_hint='+request.session["id_token"]+ \
+                               '&post_logout_redirect_uri=' + redirect_url
+    response = requests.get(url, verify=False)
+    print('response ====================>', response.status_code, response.content)
+
+    try:
+        del request.session['logged_in_username']
+        del request.session["logged_in_user_org"]
+        del request.session["logged_in_user_email"]
+        del request.session["access_token"]
+        del request.session["id_token"]
+        del request.session["state"]
+        del request.session["session_state"]
+    except KeyError:
+        pass
+
+    return HttpResponseRedirect(url)
