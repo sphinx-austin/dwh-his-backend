e93ccc1550f8026e8043d401ff8837a1c5d3495e
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-04-20 15:29:39 +0300

    updated users module

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,11 @@ from . import views
 
 urlpatterns = [
     path('', views.test, name='test'),
-    path('signin/', views.signin, name='signin'),
+    # path('signin/', views.signin, name='signin'),
     # path('connect/authorize/callback/', views.signup_callback, name='signup_callback'),
-    path('signin-oidc', views.signin_oidc, name='signin_oidc'),
-    path('logout_user/', views.logout_user, name='logout_user'),
+    # path('signin-oidc', views.signin_oidc, name='signin_oidc'),
+    # path('logout_user/', views.logout_user, name='logout_user'),
     path('store_tokens', views.store_tokens, name='store_tokens'),
-    path('log_user_in', views.log_user_in, name='log_user_in'),
+    # path('log_user_in', views.log_user_in, name='log_user_in'),
 ]
 
