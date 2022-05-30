05a5678c560c47520798dcbc9642e8c88da8211a
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-05-30 12:03:00 +0300

    update email server

--- mfl_interface/settings.py
+++ mfl_interface/settings.py
@@ -150,6 +150,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = "DWH-HIS Management Portal" #+ env("EMAIL_HOST_USER")
 EMAIL_HOST = 'smtp.gmail.com'   #'smtp.mailgun.org'
 EMAIL_HOST_USER = 'info.dwhhisplatform@gmail.com'#env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = 'trialbyfire3900' #env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
